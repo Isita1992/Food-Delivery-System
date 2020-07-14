f464b76652f761e38ac3ecdcd6371ae5f8d7b13c
Isita Amrit <47280441+Isita1992@users.noreply.github.com>
2020-07-14 13:19:01 +0530

    SB-P-Commit

---
+++ Food-Delivery-System-App/src/main/java/com/example/demo/FoodDeliverySystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliverySystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliverySystemAppApplication.class, args);
+	}
+
+}
